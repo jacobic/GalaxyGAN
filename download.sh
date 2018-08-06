5abcfcaf09a123abb9ad80e848a44b103baedf3a
jacobic <jacobic@hotmail.co.uk>
2018-08-06 14:37:04 +0200

    yapf

--- download.sh
+++ download.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+DIR=$(pwd)
+DATA_DIR=$USER/GalaxyGAN/data
+mkdir -p /u/$DATA_DIR
+mkdir -p /ptmp/$DATA_DIR
+cd /ptmp/DATA_DIR
 wget https://share.phys.ethz.ch/~blackhole/spaceml/GalaxyGAN/fitsdata.tar.gz
 tar -xvzf fitsdata.tar.gz
+cd $DIR
+ln -s /ptmp/$DATA_DIR /u/$DATA_DIR
